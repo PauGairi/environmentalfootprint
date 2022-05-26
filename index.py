601d6c9d9b64887c7b0c56818854a5bf61a0bde1
PauGairi <37814974+PauGairi@users.noreply.github.com>
2022-05-26 13:51:04 +0200

    _update_

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-	return"<h2>Flask Vercel</h2>"
+    return "<h2>This is Python working Online!! We get it Gerard!</h2>"
